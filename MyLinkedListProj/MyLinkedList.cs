62edc7da4dfd96bf5ca5c1e8065dd1af9446fbc1
Ani Grigoryan <anigrig.am@gmail.com>
2026-02-28 13:11:55 +0400

    Add project files.

---
+++ MyLinkedListProj/MyLinkedList.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+
+namespace MyLinkedListProj;
+
+public class MyLinkedList<T> : ICollection<T>
+{
+    public MyLinkedList()
+    {
+
+    }
+
+    public MyLinkedListNode<T> Head { get; private set; }
+    public MyLinkedListNode<T> Tail { get; private set; }
+
+    #region ICollection
+    public int Count { get; private set; }
+    public bool IsReadOnly { get => false; }
+
+    public void Add(T item)
+    {
+        AddFirst(item);
+    }
+
+    public void Clear()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+
+    public bool Contains(T item)
+    {
+        var current = Head;
+
+        while (current != null)
+        {
+            if (current.Value.Equals(item))
+                return true;
+
+            current = current.Next;
+        }
+
+        return false;
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        var current = Head;
+
+        while (current != null)
+        {
+            array[arrayIndex++] = current.Value;
+            current = current.Next;
+        }
+    }
+    public bool Remove(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = Head;
+
+        while (current != null)
+        {
+            yield return current.Value;
+
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    #endregion ICollection
+    #region Add
+    public void AddFirst(T value)
+    {
+        AddFirst(new MyLinkedListNode<T>(value));
+    }
+    private void AddFirst(MyLinkedListNode<T> node)
+    {
+        MyLinkedListNode<T> temp = Head;
+        
+        Head = node;
+        Head.Next = temp;
+        Count++;
+        
+        if (Count == 1)
+        {
+            Tail = Head;
+        }
+    }
+
+    public void AddLast(T value)
+    {
+        AddLast(new MyLinkedListNode<T>(value));
+    }
+    private void AddLast(MyLinkedListNode<T> node)
+    {
+        if (Count == 0)
+        {
+            Head = node;
+            Tail = node;
+        }
+        else
+        {
+            Tail.Next = node;
+            Tail = node;
+        }
+            Count++;
+    }
+    #endregion Add
+    #region Remove
+    /// NOTE: Must not use value
+    //public void RemoveFirst(T value)
+    //{
+    //    RemoveFirst(new MyLinkedListNode<T>(value));
+    //}
+    public void RemoveFirst<T>()
+    {
+        Head = Head.Next;
+        Count--;
+    }
+
+    /// NOTE: dzel
+    public void RemoveLast(T value)
+    {
+        RemoveLast(new MyLinkedListNode<T>(value));
+    }
+    private void RemoveLast(MyLinkedListNode<T> node)
+    {
+        if (Count != 0)
+        {
+            if (Count == 1)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else
+            {
+                MyLinkedListNode<T> current = Head;
+
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = null;
+                Tail = current;
+            }
+            Count--;
+        }
+    }
+    #endregion Remove
+}
